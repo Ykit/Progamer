56529cc8ad1e66fd147c010d6cceadb0363eb26e
Steven Tai <s1107176@s.ied.edu.hk>
2016-02-29 19:42:26 +0800

    start

---
+++ Assets/_func/infoInputScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEditor;
+
+
+
+public class infoInputScript : MonoBehaviour {
+	public string username;
+	private string password;
+	public Text alert;
+	void Start (){
+		alert.text = "";
+	}
+	public void resetAlert(){
+		alert.text = "";
+	}
+
+	public void UsernameInput(InputField usernameInput){
+		username = usernameInput.text;
+
+	}
+
+	public void PasswordInput(InputField passwordInput){
+		password = passwordInput.text;
+	}
+
+	public void login(){
+		if (username=="admin" && password=="admin"){
+			Application.LoadLevel("user");
+		}else{
+			alert.text = "Wrong username or password";
+		}
+	}
+
+}
+//http://wiki.unity3d.com/index.php?title=Server_Side_Highscores
+//http://forum.unity3d.com/threads/starting-my-mmo-with-sql-server-in-javascript.77447/
+//https://www.youtube.com/watch?v=Q5dCbuGzZow
